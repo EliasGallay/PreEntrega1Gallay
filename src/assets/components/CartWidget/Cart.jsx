a6b48db15aed28d547d0617a5ebe5fb34b4e98b5
Elias Gallay <eliasmgallay@gmail.com>
2025-03-24 14:54:31 -0300

    add hover

--- src/assets/components/CartWidget/Cart.jsx
+++ src/assets/components/CartWidget/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
+import './Cart.css'
 
 const Cart = () => {
     const [cart, setCart] = useState(0)
   return (
     <div onClick={ () => setCart(cart + 1)}>
-       <>🛒</>
+       <span className='cart-icon'>🛒</span>
        <span>{cart}</span>
     </div>
   )
